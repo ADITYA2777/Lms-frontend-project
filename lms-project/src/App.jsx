ef9b2584453ef6e3667d17d6d0c82abef83ccf3d
ADITYA2777 <ajadityajain01@gmail.com>
2023-10-19 11:21:52 -0700

    NotFoundPage

--- lms-project/src/App.jsx
+++ lms-project/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/about" element={<AboutPage/>}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        
+        <Route path="*"element={<NotFoundPage/>}></Route>
      </Routes>
     </>
   );
