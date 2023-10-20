c1f683f345ddc6cbbc10b2e381eacd400c1da8c3
ADITYA2777 <ajadityajain01@gmail.com>
2023-10-20 11:54:30 -0700

    Singup

--- lms-project/src/App.jsx
+++ lms-project/src/App.jsx
@@ -2,15 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
 import NotFoundPage from "./Pages/NotFoundPage";
+import SingupPage from "./Pages/SingupPage";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutPage />}></Route>
-        
-        <Route path="*"element={<NotFoundPage/>}></Route>
-     </Routes>
+        <Route path="/signup" element= {<SingupPage/>}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
+      </Routes>
     </>
   );
 }
